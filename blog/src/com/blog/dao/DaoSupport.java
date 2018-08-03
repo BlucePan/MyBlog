84950782715904bab2ccf71a2a5596d1ca533f5f
BlucePan <15527668722@163.com>
2018-08-03 09:42:31 +0800

    111

---
+++ blog/src/com/blog/dao/DaoSupport.java
@@ -0,0 +1,78 @@
+package com.blog.dao;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.apache.ibatis.session.ExecutorType;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.blog.Dao;
+/**
+ * 无实体类通用操作数据库方法
+ * @author  panzhi
+ * @date    2018年8月1日
+ * @version 1.0.0
+ */
+@Repository("daoSupport")
+public class DaoSupport implements Dao {
+
+	@Resource(name = "sqlSessionTemplate")
+	private SqlSessionTemplate sqlSessionTemplate;
+	
+	public Object save(String str, Object obj) throws Exception {
+		return sqlSessionTemplate.insert(str, obj);
+	}
+	
+	public Object batchSave(String str, List objs )throws Exception{
+		return sqlSessionTemplate.insert(str, objs);
+	}
+	
+	public Object update(String str, Object obj) throws Exception {
+		return sqlSessionTemplate.update(str, obj);
+	}
+
+	public void batchUpdate(String str, List objs )throws Exception{
+		SqlSessionFactory sqlSessionFactory = sqlSessionTemplate.getSqlSessionFactory();
+		//批量执行器
+		SqlSession sqlSession = sqlSessionFactory.openSession(ExecutorType.BATCH,false);
+		try{
+			if(objs!=null){
+				for(int i=0,size=objs.size();i<size;i++){
+					sqlSession.update(str, objs.get(i));
+				}
+				sqlSession.flushStatements();
+				sqlSession.commit();
+				sqlSession.clearCache();
+			}
+		}finally{
+			sqlSession.close();
+		}
+	}
+	
+	public Object batchDelete(String str, List objs )throws Exception{
+		return sqlSessionTemplate.delete(str, objs);
+	}
+	
+	public Object delete(String str, Object obj) throws Exception {
+		return sqlSessionTemplate.delete(str, obj);
+	}
+	 
+	public Object findForObject(String str, Object obj) throws Exception {
+		return sqlSessionTemplate.selectOne(str, obj);
+	}
+
+	public Object findForList(String str, Object obj) throws Exception {
+		return sqlSessionTemplate.selectList(str, obj);
+	}
+	
+	public Object findForMap(String str, Object obj, String key, String value) throws Exception {
+		return sqlSessionTemplate.selectMap(str, obj, key);
+	}
+	
+}
+
+
